b38c5841ff5bed6600e805c89b995ad8ed91c9ae
PICK IT UP <angelsharma629622@gmail.com>
2018-10-27 03:13:59 +0600

    Initial commit

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{config("app.name", "Title not found")}}</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <h1>Welcome to Laravel</h1>
+    <p>This is second run on PHPStorm</p>
+</body>
+</html>
---
+++ resources/views/pages/services.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{config("app.name", "Title not found")}}</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+</head>
+<body>
+<h1>Services</h1>
+<ul>
+    <li>Java</li>
+    <li>PHP</li>
+    <li>Android</li>
+</ul>
+<p>This is second run on PHPStorm</p>
+</body>
+</html>
